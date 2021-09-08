056f47da4cbaae842770ba46fa9d8cb6f1c9ea4a
Gerren <g.ballard23@outlook.com>
2021-09-08 11:58:17 -0500

    commit tree 4f6db4fa602e0d3584fef12298000464aa51f349

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, str){
+    return array.filter(item => item.toUpperCase() === str.toUpperCase())
+
+}
+
+function fuzzyMatch(array, str){
+    return array.filter(item => item[0] === str[0])
+}
+
+function matchName(array, str){
+    return array.filter(item => item.name === str)
+}
